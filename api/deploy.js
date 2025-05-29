81458fe86abcc96b0a7939cb10717188b973d283
ngaudiomanychat <nick.gaudio@manychat.com>
2025-05-29 15:11:27 -0500

    Update deploy.js

--- api/deploy.js
+++ api/deploy.js
@@ -1,4 +1,4 @@
-javascript// api/deploy.js
+// api/deploy.js
 export default function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' });
